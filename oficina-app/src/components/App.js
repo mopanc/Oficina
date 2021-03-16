b90b6e4d40e911ff9950c9fa89dcb01aeb78a949
mopanc <jorgemopanc@icloud.com>
2021-03-16 20:20:50 -0300

    #3 Add main buttons to NavBar on Dashboard

--- oficina-app/src/components/App.js
+++ oficina-app/src/components/App.js
@@ -1,55 +1,37 @@
 import { Layout, Menu, Breadcrumb } from 'antd';
-import {
-  DesktopOutlined,
-  PieChartOutlined,
-  FileOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
+import { Button } from 'antd';
+import { DashboardOutlined, MailOutlined } from '@ant-design/icons';
 import { useState } from 'react';
 import './App.css';
 
-const { Header, Content, Footer, Sider } = Layout;
+const { Header, Content, Footer } = Layout;
 const { SubMenu } = Menu;
 
 function App () {
   const [collapsed, setCollapsed] = useState(false);
 
-  return <Layout style={{ minHeight: '100vh' }}>
-    <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
-      <div className="logo" />
-      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-        <Menu.Item key="1" icon={<PieChartOutlined />}>
-          Option 1
-        </Menu.Item>
-        <Menu.Item key="2" icon={<DesktopOutlined />}>
-          Option 2
-        </Menu.Item>
-        <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-          <Menu.Item key="3">Tom</Menu.Item>
-          <Menu.Item key="4">Bill</Menu.Item>
-          <Menu.Item key="5">Alex</Menu.Item>
-        </SubMenu>
-        <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-          <Menu.Item key="6">Team 1</Menu.Item>
-          <Menu.Item key="8">Team 2</Menu.Item>
-        </SubMenu>
-        <Menu.Item key="9" icon={<FileOutlined />}>
-          Files
-        </Menu.Item>
-      </Menu>
-    </Sider>
-    <Layout className="site-layout">
-      <Header className="site-layout-background" style={{ padding: 0 }} />
-      <Content style={{ margin: '0 16px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>User</Breadcrumb.Item>
-          <Breadcrumb.Item>Bill</Breadcrumb.Item>
-        </Breadcrumb>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-    </Layout>
-  </Layout>
+  return <Layout className="layout">
+  <Header>
+
+    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+      <Button type="primary" shape="round" icon={<MailOutlined />} style={{ margin: '0 50px' }} size={10}>
+        Agendar Serviço
+      </Button>
+      <Button type="primary" shape="round" icon={<DashboardOutlined />} size={10}>
+        Administração
+      </Button>
+    </Menu>
+  </Header>
+  <Content style={{ padding: '0 50px' }}>
+    <Breadcrumb style={{ margin: '16px 0' }}>
+      <Breadcrumb.Item>Home</Breadcrumb.Item>
+      <Breadcrumb.Item>List</Breadcrumb.Item>
+      <Breadcrumb.Item>App</Breadcrumb.Item>
+    </Breadcrumb>
+    <div className="site-layout-content">Content</div>
+  </Content>
+  <Footer style={{ textAlign: 'center' }}>Ant Design ©2021 Created by Ant UED</Footer>
+</Layout>
 }
 
 export default App
