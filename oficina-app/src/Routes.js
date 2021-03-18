e08f5f6e8de6e67c0f596dd36b0f32b6f61c5d73
mopanc <jorgemopanc@icloud.com>
2021-03-18 18:05:06 -0300

    #6 #8 Add Router/Axios

---
+++ oficina-app/src/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Nav from './components/Nav';
+import App from './App';
+import Admin from './components/Administrator/Administrator'
+import './App.css';
+import 'antd/dist/antd.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Nav />
+    <Switch>
+      <Route path="/" exact={true} component={App} />
+      <Route path="/administrators" component={Admin} />
+    </Switch>
+  </ BrowserRouter>
+  , document.getElementById('root'));
